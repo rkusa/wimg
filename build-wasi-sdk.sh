9ef36a563f3086c5026edff00ecbf14e9ae18fc0
Markus Ast <m@rkusa.st>
2022-02-04 21:27:01 +0100

    fix build-wasi-sdk.sh script

--- build-wasi-sdk.sh
+++ build-wasi-sdk.sh
@@ -2,6 +2,6 @@
 set -e
 git clone --branch wasi-sdk-14 https://github.com/WebAssembly/wasi-sdk.git
 cd wasi-sdk
-git submodule update --init --recursive --depth 1
+git submodule update --init --recursive
 NINJA_FLAGS=-v make package
 tar -xzf dist/wasi-sdk-*.tar.gz -C dist
